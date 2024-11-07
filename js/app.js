dc025e3d9b965bde345605a6dbccd74764dd418a
Staasie <ana.stay.stay@gmail.com>
2024-11-07 15:42:20 -0800

    update

--- js/app.js
+++ js/app.js
@@ -27,10 +27,10 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function multiply(a, b) { //eslint-disable-line
-//   let second = a +b;
-//   return [second, `the sum of ${a} and ${b} is ${second}`]
-// }
+function multiply(a, b) { //eslint-disable-line
+  let second = a +b;
+  return [second, `the sum of ${a} and ${b} is ${second}`]
+}
 
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
